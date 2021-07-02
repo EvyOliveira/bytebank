c667a5a60dfb9e0b8c79fb4aaccb706c11fa1f2e
Evelyn Cristini Oliveira <evelyn.oliveira@sensedia.local>
2021-07-02 14:14:59 -0300

    Refactor select and initialize variables

--- sintaxe-variaveis/src/alura/TestaEscopoVariaveis.java
+++ sintaxe-variaveis/src/alura/TestaEscopoVariaveis.java
@@ -1,14 +1,14 @@
 package alura;
 
-public class TestaEscopo {
+public class TestaEscopoVariaveis {
 	public static void main(String[] args) {
 		System.out.println("Testando condicionais");
-		
+
 		int idade = 16;
 		int quantidadePessoas = 3;
 		boolean acompanhado;
-		
-		if(quantidadePessoas >= 2) {
+
+		if (quantidadePessoas >= 2) {
 			acompanhado = true;
 		} else {
 			acompanhado = false;
