ac7d998482c25de91a4cd126e56db7b8d23c65b6
Evelyn Cristini Oliveira <evelyn.oliveira@sensedia.local>
2021-07-27 18:18:54 -0300

    kkkkk

---
+++ sintaxe-variáveis-e-fluxo/src/programs/TestaVariavel.java
@@ -0,0 +1,18 @@
+package programs;
+
+public class TestaVariavel {
+	
+	public static void main(String[] args) {
+		System.out.println("Olá novo teste!");
+		
+		int idade;
+		idade = 24;
+		
+		System.out.println(idade);
+		
+		idade = 30 + 10;
+		idade = (7 * 5) + 2;
+		
+		System.out.println("A idade é " + idade);
+	}
+}
