1e1538c537986757e72a13c2b8b2f05d0d647128
Evelyn Cristini Oliveira <evelyn.oliveira@sensedia.local>
2021-07-27 12:28:08 -0300

    Initialize Java project

---
+++ bytebankII/src/bytebank/TesteContas.java
@@ -0,0 +1,18 @@
+package bytebank;
+
+public class TesteContas {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Conta conta = new Conta(1337, 23334);
+		conta.deposita(200.0);
+		
+		System.out.println(conta.getSaldo());
+		
+		conta.setAgencia(570);
+		
+		System.out.println(conta.getAgencia());
+		System.out.println("O total de contas é: " + Conta.getTotal());
+	}
+	
+}
