a181c1f098b861f76096a70fa10ae43e4004004e
Evelyn Cristini Oliveira <evelyn.oliveira@sensedia.local>
2021-08-17 16:19:29 -0300

    Add output message in getBonificacao method

--- bytebank-herdado/src/bytebank/EditorDeVideo.java
+++ bytebank-herdado/src/bytebank/EditorDeVideo.java
@@ -3,6 +3,7 @@ package bytebank;
 public class EditorDeVideo extends Funcionario {
 	
 	public double getBonificacao() {
+		System.out.println("Chamando o método getBonificacao do EDITOR DE VIDEO.");
 		return super.getBonificacao() + 100;
 	}
 }
