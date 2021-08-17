05eb40bfbc45904a672d4da0885a0c2c803c7af8
Evelyn Cristini Oliveira <evelyn.oliveira@sensedia.local>
2021-08-17 15:00:43 -0300

    Refactor reference tests for employee bonuses

--- bytebank-herdado/src/bytebank/TesteReferencias.java
+++ bytebank-herdado/src/bytebank/TesteReferencias.java
@@ -4,14 +4,26 @@ public class TesteReferencias {
 
 	public static void main(String[] args) {
 		
-		Funcionario g1 = new Gerente();
+		Gerente g1 = new Gerente();
 		g1.setNome("Marilia");
 		String nome = g1.getNome();
-		
+		g1.setSalario(5000.0);
 		//g1.autentica(2222);
 		
 		System.out.println("O nome do(a) gerente é " + nome + ".");
-
+		
+		Funcionario f = new Funcionario();
+		f.setSalario(2000.0);
+		
+		EditorDeVideo e = new EditorDeVideo();
+		e.setSalario(2500.0);
+		
+		ControleBonificacao controle = new ControleBonificacao();
+		controle.registra(g1);
+		controle.registra(f);
+		controle.registra(e);
+		
+		System.out.println("O salário com a bonificação de um gerente é de " + controle.getSoma() + " reais.");
 	}
 
 }
